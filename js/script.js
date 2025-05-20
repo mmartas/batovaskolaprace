a9531ca46f4852cbd3d853c26d9c54ea93b9c3f9
Martin Šupler <martin.supler@seznam.cz>
2025-05-20 22:51:15 +0200

    .

--- js/script.js
+++ js/script.js
@@ -5,3 +5,27 @@ hamburgerMenu.addEventListener("click", function(event){
     hamburgerMenu.classList.toggle("active")
     navBarMenu.classList.toggle("active")
 })
+
+let galleryRightArrow = document.querySelector(".fa-solid.fa-arrow-right")
+
+let galleryLeftArrow = document.querySelector(".fa-solid.fa-arrow-left")
+
+let galleryLine = document.querySelector(".gallery_photos")
+
+let pixelNumber = 0
+
+galleryRightArrow.addEventListener("click", function(event){
+    if(pixelNumber > -3290){
+        pixelNumber -= 140
+        galleryLine.style.transform = `translateX(${pixelNumber}px)`
+    }
+    
+})
+
+galleryLeftArrow.addEventListener("click", function(event){
+    if(!pixelNumber == 0){
+        pixelNumber += 140
+        galleryLine.style.transform = `translateX(${pixelNumber}px)`
+    }
+    
+})
